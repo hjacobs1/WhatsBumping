95740edcf50216029716f6ab6de444bf9e3436d4
ehardacre <hardacre.ethan@gmail.com>
2018-12-09 13:45:16 +0100

    Recycler View half done

---
+++ app/src/main/java/com/example/henryjacobs/whatsbumping/adapter/FeedAdapter.kt
@@ -0,0 +1,13 @@
+package com.example.henryjacobs.whatsbumping.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+class FeedAdapter : RecyclerView.Adapter<FeedAdapter.ViewHolder> {
+
+
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    }
+}
